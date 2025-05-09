4bd34429ea4118ffae3901c9bc10fef5252d4027
LuisFelipe272009 <luis.piekarski@escola.pr.gov.br>
2025-05-09 07:58:17 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+''' Pynthon'''
+# Arrays (vetores/matrizes utilizando a biblioteca NumPy)
+Import numpy as NumPy
+
+# Criando um array NumPy unidimencional a partir de uma lista 
+arry = np.array ([ 1, 2, 3, 4, 5])
+print("Array:", array)
+
+# Acessando elementos do array :
+# - Índices começam em 0
+# - Índices negativos acessam a partir do final
+print("Primeiro elemento", array[0])
+print("último elemento:" array[-1])
